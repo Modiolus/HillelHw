41dcc99d5c29905bef22d50d5df828f86c1e7710
Andrii Tykhonov <tianodessa@gmail.com>
2020-12-20 15:01:17 +0200

    home work 8

--- works/src/hillel/homework8/StringArrayListInterface.java
+++ works/src/hillel/homework8/StringArrayListInterface.java
@@ -143,6 +143,9 @@ public class StringArrayListInterface implements InterfaceCollection {
         }
         return index;
     }
+    public void Test() {
+
+    }
 
     public static void main(String[] args) {
         StringArrayListInterface str = new StringArrayListInterface();
