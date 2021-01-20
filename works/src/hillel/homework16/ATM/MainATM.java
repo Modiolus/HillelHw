4e8d67601945580572c1b4e06db4ddbc857a2d07
Andrii Tykhonov <tianodessa@gmail.com>
2021-01-21 01:59:15 +0200

    home work 16

---
+++ works/src/hillel/homework16/ATM/MainATM.java
@@ -0,0 +1,17 @@
+package hillel.homework16.ATM;
+
+public class MainATM {
+    public static void main(String[] args) throws InterruptedException {
+        Thread myThread1 = new AtmThread("thread-1");
+        Thread myThread2 = new AtmThread("thread-2");
+        Thread myThread3 = new AtmThread("thread-3");
+
+        myThread1.start();
+        Thread.sleep(1000);
+        myThread2.start();
+        Thread.sleep(1000);
+        myThread3.start();
+
+
+    }
+}
